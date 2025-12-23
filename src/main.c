f0b22029bd93dd06e8126cdb0380b27d4218263a
Repik <jakub.martinak@outlook.com>
2025-12-24 00:03:23 +0100

    base getopt library usage

--- src/main.c
+++ src/main.c
@@ -8,8 +8,39 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n - creates new database file\n");
+  printf("\t -f - (required) path to the database file\n");
+  return;
 }
 
 int main(int argc, char *argv[]) { 
-	
+	bool new_file = false;
+  int c = 0;
+  char *file_path = NULL;
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) {
+    switch (c) {
+      case 'n':
+        new_file = true;
+        break;
+      case 'f':
+        file_path = optarg;
+        break;
+      case '?':
+        printf("Unknown flag -%c\n", c);
+        break;
+      default:
+        return -1;
+    }
+  }
+  if (file_path == NULL) {
+    printf("File_path is a required argument\n");
+    print_usage(argv);
+    return EXIT_SUCCESS;
+  }
+  printf("new_file: %d\n", new_file);
+  printf("file_path: %s\n", file_path);
+
+  return EXIT_SUCCESS;
 }
